7f6793cb07889c1ab461d856383b640781c41b1f
Leon Mak <leonmakansheng@gmail.com>
2017-10-07 13:58:55 -0700

    Initial Commit

---
+++ Watson-Tomorrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watson-Tomorrow
+//
+//  Created by Leon Mak on 7/10/17.
+//  Copyright © 2017 Leon Mak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
